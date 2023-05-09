52bae357cf74048e2ae451cca4d947ca783672bc
kengu7272 <kengu72@gmail.com>
2023-05-09 02:14:11 -0400

    started mobile page for home

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,17 @@
 import Login from '../pages/login/Login'
+import Home from '../pages/home/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,34 +1,42 @@
 import logo from './discord-logo-white.png'
 import qr from './qr.png'
+import {Link} from "react-router-dom";
 
 function Login() {
     return (
     <div className='w-full h-full bg-zinc-800 text-white text-sm
-    desktop:flex desktop:flex-row desktop:justify-center desktop:items-center desktop:bg-[url("/src/pages/login/background.jpg")]'>
+    tablet:flex tablet:flex-row tablet:justify-center tablet:items-center tablet:bg-[url("/src/pages/login/background.jpg")]'>
+
         <div className='flex flex-col items-center gap-1
-        desktop:hidden'>
+        tablet:hidden'>
             <img src={ logo } alt="Discord Logo" className='w-1/4 my-4' /> 
             <h3 className='mb-2'>Welcome back!</h3>
             <p className='text-gray-400'>We're so excited to see you again!</p>
         </div>
         
         <div className='h-3/5
-        desktop:flex desktop:flex-row desktop:w-[40%] desktop:h-[45%] desktop:bg-zinc-800 desktop:rounded-lg'>
+        tablet:flex tablet:flex-row tablet:w-5/6 tablet:h-[45%] tablet:bg-zinc-800 tablet:rounded-lg
+        laptop:w-3/4
+        desktop:w-2/5'><Link to="/Home">Home</Link>
             <form className='mx-4 text-xs text-gray-200 h-full
-            desktop:w-3/5 desktop:mx-8 desktop:flex desktop:flex-col desktop:justify-center'>
-                <div className='hidden desktop:block text-center text-base'>
+            tablet:w-3/5 tablet:mx-8 tablet:flex tablet:flex-col tablet:justify-center'>
+
+                <div className='hidden tablet:block text-center text-base'>
                     <h3 className='mb-2'>Welcome back!</h3>
                     <p className='text-gray-400'>We're so excited to see you again!</p>
                 </div>
+                
                 <p className='mt-3 mb-1'>EMAIL OR PHONE NUMBER <span className='text-red-500'>*</span></p>
-                <input type="text" className='w-full rounded-sm h-[8%] bg-zinc-900 text-base
-                desktop:h-[9.5%]' />
+                <input type="text" className='w-full rounded-sm h-[8%] bg-zinc-900
+                tablet:h-[9.5%] tablet:text-xl'/>
+
                 <p className='mt-5 mb-1'>PASSWORD <span className='text-red-500'>*</span></p>
-                <input type="password" className='w-full rounded-sm h-[8%] bg-zinc-900 text-base
-                desktop:h-[9.5%]'/>
+                <input type="password" className='w-full rounded-sm h-[8%] bg-zinc-900
+                tablet:h-[9.5%] tablet:text-xl'/>
+
                 <p className='text-blue-400 mt-1 mb-4'>Forgot your password?</p>
                 <button className='bg-indigo-600 w-full h-[6%] rounded-sm
-                desktop:h-[9.5%]'>
+                tablet:h-[9.5%]'>
                     Log In
                 </button>
                 <br></br>
@@ -36,7 +44,7 @@ function Login() {
             </form>
 
             <div className='hidden text-center flex-col justify-center items-center
-            desktop:flex'>
+            tablet:flex'>
                 <img src={ qr } alt="QR Code" className='h-48 w-48'/>
                 <h2 className='mb-2 mt-8'>Log in with QR Code</h2>
                 <p className='text-gray-400'>Scan this with the <span className='font-bold'>Discord mobile app</span> to log in instantly.</p>
@@ -46,4 +54,4 @@ function Login() {
     );
 }
 
-export default Login
+export default Login;
