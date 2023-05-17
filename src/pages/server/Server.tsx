146bb87bb5892f6e4f46a6963b82028faae3aba1
kengu7272 <kengu72@gmail.com>
2023-05-16 21:23:07 -0400

    fixed top left home button

--- src/pages/server/Server.tsx
+++ src/pages/server/Server.tsx
@@ -1,15 +1,21 @@
 import ServerChannelsBar from "../../components/ServerChannelsBar";
 import ServerBar from "../../components/ServerBar";
-
+import { useNavigate } from "react-router-dom";
 
 interface ServerProps {
     name: string;
 }
 
 const Server: React.FC<ServerProps> = ({name}) => {
+    const navigate = useNavigate();
+
+    function ToHome() {
+        navigate('/Home');
+    }
+
     return (
         <div className="w-full h-full flex flex-row">
-            <ServerBar/>
+            <ServerBar func={ToHome}/>
             <ServerChannelsBar name={name} />
         </div>
     );
