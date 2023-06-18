8ced2603f1172ea56acc3f3a1de2142e6816fc5c
kengu7272 <kengu72@gmail.com>
2023-06-18 16:11:42 -0400

    Started messaging screens
    Few changes adding the layout for messaging screen

--- src/pages/server/Server.tsx
+++ src/pages/server/Server.tsx
@@ -1,6 +1,13 @@
 import ServerChannelsBar from "../../components/ServerChannelsBar";
 import ServerBar from "../../components/ServerBar";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+function UploadMessage() {
+    return (
+        <h4>Message</h4>
+    );
+}
 
 interface ServerProps {
     name: string;
@@ -13,10 +20,35 @@ const Server: React.FC<ServerProps> = ({name}) => {
         navigate('/Home');
     }
 
+    const [toggle, setToggle] = window.innerWidth >= 640 ? useState(2) : useState(1);
+
+    function checkToggle() {
+        setToggle(toggle == 1 ? 0 : toggle == 0 ? 1 : 2);
+    }
+
     return (
         <div className="w-full h-full flex flex-row">
-            <ServerBar func={ToHome}/>
-            <ServerChannelsBar name={name} />
+            <div  style={{display: toggle == 0 ? 'none' : 'flex'}}>
+                <ServerBar func={ToHome}/>
+                <ServerChannelsBar name={name} func={checkToggle} />
+            </div>
+            
+            <div style={{display: toggle == 0 || toggle == 2 ? 'flex' : 'none'}} className="flex-col items-center hidden text-gray-400 w-full h-full">
+                <div className="flex flex-row justify-center items-center">
+                    <button onClick={checkToggle} className="left-2 absolute laptop:hidden">=</button>
+                    <h2>general</h2>
+                </div>
+
+                <div className='bg-zinc-900 w-full h-[2px] mb-4'/>
+
+                <div className="w-full h-full p-4 relative flex flex-col items-center
+                laptop:min-w-0 laptop:flex-grow">
+                    <form className="h-[4.5vh] flex flex-row items-center justify-center gap-4 bottom-4 absolute w-full">
+                        <input className="w-5/6 px-2 h-full bg-zinc-700" placeholder="Enter a message"></input>
+                        <button onClick={UploadMessage} className="text-3xl">+</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
